852799df60c664c5d35cb8f69b62f7ff8494f59b
Alex-Techy60 <sugyanworkplace@gmail.com>
2026-02-24 16:19:53 +0530

    Converted 01basicreact from subrepo to normal folder

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <h1>
+      The Group made of love and teasing. 
+    </h1>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Friends.jsx
@@ -0,0 +1,16 @@
+function Friends() {
+  return (
+    <>
+      <ul>
+        <li>Olivia Winters</li>
+        <li>Jake Redcliff</li>
+        <li>Chris Rogers</li>
+        <li>Emma Carter</li>
+        <li>Anna Stones</li>
+        <li>Alex McDominey</li>
+      </ul>
+    </>
+  );
+}
+
+export default Friends;
